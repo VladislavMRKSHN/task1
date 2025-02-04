1510a32e1214b86967abf8b364c9715c5f19a394
Владислав <vmarkushin04@mail.ru>
2025-02-04 12:37:00 +0700

    Исправлены ошибки в Task1 и Task2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        double r;
+        double pi = 3.14;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Запрос радиуса
+        System.out.print("Введите радиус круга: ");
+        r = scanner.nextDouble();
+
+        // Проверка, чтобы радиус был положительным
+        if (r < 0) {
+            System.out.println("Радиус не может быть отрицательным. Введите положительное значение.");
+            return;
         }
+
+        // Вычисление формулы
+        double s = pi * r * r;
+
+        // Вывод результата
+        System.out.println("Площадь круга с радиусом " + r + " равна " + s);
     }
 }
